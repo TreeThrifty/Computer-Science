d7ff1ee60da2dfcb93c25e5ae1deacb92798a9aa
TreeThrifty <131738467+TreeThrifty@users.noreply.github.com>
2024-02-16 17:19:23 -0500

    Thanks for editing!

--- lab_assignment_5.c
+++ lab_assignment_5.c
@@ -6,39 +6,85 @@ typedef struct node {
 	struct node* next;
 } node;
 
-// Returns number of nodes in the linkedList.
+// returns the length of the linkedList
 int length(node* head)
 {
-   struct node *tmp = head;
-    int len = 0;
-   while (tmp != NULL)
-   {
-      tmp = tmp->next;
-      len++;
-   }
+	int count = 0;
+	node* current = head;
+	while (current != NULL)
+	{
+		count++;
+		current = current->next;
+	}
+	return count;
 
-   return (len);
 }
 
 // parses the string in the linkedList
 //  if the linked list is head -> |a|->|b|->|c|
 //  then toCString function wil return "abc"
+
 char* toCString(node* head)
 {
+	int len = length(head);
+	char* str = (char*)malloc(len + 1);
+	node* current = head;
+	int i = 0;
+	while (current != NULL)
+	{
+		str[i] = current->letter;
+		current = current->next;
+		i++;
+	}
+	str[i] = '\0';
+	return str;
 }
 
-// inserts character to the linkedlist
-// f the linked list is head -> |a|->|b|->|c|
-// then insertChar(&head, 'x') will update the linked list as foolows:
+
+// inserts character to the linked list
+// if the linked list is head -> |a|->|b|->|c|
+// then insertChar(&head, 'x') will update the linked list as follows:
 // head -> |a|->|b|->|c|->|x|
+
 void insertChar(node** pHead, char c)
 {
+	node* newNode = (node*)malloc(sizeof(node));
+	newNode->letter = c;
+	newNode->next = NULL;
+	if (*pHead == NULL)
+	{
+		*pHead = newNode;
+	}
+	else
+	{
+		node* current = *pHead;
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = newNode;
+	}
+
+
+
+
 }
 
 // deletes all nodes in the linkedList.
+
 void deleteList(node** pHead)
 {
+	node* current = *pHead;
+	node* next;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	*pHead = NULL;
 }
+
 
 int main(void)
 {
