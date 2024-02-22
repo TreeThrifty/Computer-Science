2f9f7a9d3396a00ac4db68bf9454008860b683de
TreeThrifty <131738467+TreeThrifty@users.noreply.github.com>
2024-02-21 21:55:25 -0500

    Add files via upload

---
+++ wheretositb.c
@@ -0,0 +1,150 @@
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define MAX_N 10
+
+int n, p; //number of friends = n and number of pairs tht cant sit together = p
+char friends[MAX_N][20]; //array of friends 
+bool alreadyUsed[MAX_N]; //array of alreadyUsed friends
+int popcorn[MAX_N]; //array of popcorn
+char seating[MAX_N][20]; 
+int adjacent[MAX_N][MAX_N];
+
+
+
+
+
+
+
+bool is_valid(int index, int last_index) {
+    if (last_index == -1)
+        return true;
+    int x = index, z = last_index;
+    if (adjacent[x][z] || adjacent[z][x])
+        return false;
+    return true;
+}
+
+
+
+
+
+
+
+bool seating_Combinations(int index, int last_index) { //function to generate the seating arrangement
+    if (index == n) {
+        return true;
+    }
+    
+    for (int i = 0; i < n; i++) { //for loop to iterate through the friends stops at n
+        if (!alreadyUsed[i] && is_valid(i, last_index)) {
+            alreadyUsed[i] = true; //set the alreadyUsed array to true
+            strcpy(seating[index], friends[i]); //copy the friends into the seating array
+            if (seating_Combinations(index + 1, i)) { //recursive call to generate the seating arrangement
+                return true;
+            }
+            alreadyUsed[i] = false; //set the alreadyUsed array to false
+        }
+    }
+    return false;
+}
+
+
+
+
+
+
+
+int main(int argc, char *argv[]) //main function to read from the file
+{
+    if(argc !=2){
+        printf("Usage: %s <input file>\n", argv[0]);
+        return 1;
+    }
+    //open the file for reading from the command line
+    FILE *fp = fopen(argv[1], "r");
+    if(fp == NULL){
+        printf("Unable to open file for reading\n");
+        return 1;
+    }
+
+                          
+ fscanf(fp, "%d %d", &n, &p); //scan values for the first two variables
+
+    
+    for (int i = 0; i < n; i++) { //scan from file until n is reached
+        fscanf(fp, "%s %d", friends[i], &popcorn[i]);
+
+    }
+
+    for (int i = 0; i < p; i++) {
+        char name1[20], name2[20];
+        //scan from file until p is reached
+    }
+{
+    if(argc !=2){
+        printf("Usage: %s <input file>\n", argv[0]);
+        return 1;
+    }
+    //open the file for reading from the command line
+    FILE *fp = fopen(argv[1], "r");
+    if(fp == NULL){
+        printf("Unable to open file for reading\n");
+        return 1;
+    }
+
+
+
+//scan values for the first two variables
+    fscanf(fp, "%d %d", &n, &p);
+    for (int i = 0; i < n; i++) {
+        //scan from file until n is reached
+        fscanf(fp, "%s %d", friends[i], &popcorn[i]);
+
+    }
+
+    for (int i = 0; i < p; i++) {
+        char name1[20], name2[20];
+        //scan from file until p is reached
+        fscanf(fp, "%s %s", name1, name2);
+
+        int x, z;  
+        for (int j = 0; j < n; j++) { //for loop to iterate through the friends stops at n
+            if (strcmp(friends[j], name1) == 0) x = j; //if the name is found in the array set x to j
+            if (strcmp(friends[j], name2) == 0) z = j; //if the name is found in the array set z to j
+        }
+        adjacent[x][z] = adjacent[z][x] = 1; 
+    }
+
+    memset(alreadyUsed, false, sizeof(alreadyUsed));  
+
+
+
+
+    
+
+    if (seating_Combinations(0, -1)) { 
+        for (int i = 0; i < n; i++) { // the loop stops after the first lexicographical order is found
+            printf("%s\n", seating[i]); //prints first ordering
+            
+
+      
+
+
+
+
+        }
+    }
+ 
+
+    return 0;
+}
+}
+
+
+
+
+ 
